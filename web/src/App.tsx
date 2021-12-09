cf80bb7172a6b67506933a777bee7e4f48febbf4
Soriphen <57924151+Soriphen@users.noreply.github.com>
2021-12-09 00:21:06 -0500

    initial commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,29 @@
 import React from "react";
+import ReposList from "./components/ReposList";
+import LangFilter from "./components/LangFilter";
 import "./App.css";
 
 export function App() {
   const reposRef = React.useRef(null);
   const [repos, setRepos] = React.useState(null);
-  const [seeArchive, setSeeArchive] = React.useState(false);
   const [isLoaded, setIsLoaded] = React.useState(false);
   const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
     const fetchRepos = () => {
-      fetch("/repos")
+      fetch("/repos") // Fetch the repos from the API endpoint created with the express app
         .then((res) => res.json())
         .then((data) => {
           if (data.status !== 400) {
-            reposRef.current = data;
-            setRepos(data);
+            reposRef.current = data.sort((a, b) => {
+              // Sorting the repos creation date by reverse chronological order
+              return a.created_at > b.created_at
+                ? -1
+                : a.created_at < b.created_at
+                ? 1
+                : 0;
+            });
+            setRepos(reposRef.current);
           } else {
             setError(data);
           }
@@ -26,8 +34,6 @@ export function App() {
     setIsLoaded(true);
   }, []);
 
-  console.log(repos, error);
-
   if (error) {
     return <div>{error.message}</div>;
   } else if (!isLoaded) {
@@ -35,7 +41,12 @@ export function App() {
   } else {
     return (
       <div className="App">
-        <header className="App-header">hello</header>
+        {repos && (
+          <>
+            <LangFilter reposRef={reposRef} repos={repos} setRepos={setRepos} />
+            <ReposList reposRef={reposRef} repos={repos} setRepos={setRepos} />
+          </>
+        )}
       </div>
     );
   }
